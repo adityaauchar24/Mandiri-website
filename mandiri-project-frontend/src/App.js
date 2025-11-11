f5d10efd1792fbf4ad2dae276e9a89591081be7f
AdityaAuchar <aucharsujata@gmail.com>
2025-11-11 12:59:21 +0530

    Emergency fix: minimal dependencies for deployment

--- mandiri-project-frontend/src/App.js
+++ mandiri-project-frontend/src/App.js
@@ -1,6 +1,5 @@
 import { Route, BrowserRouter as Router, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-import "@fontsource/roboto";
 import AboutUs from './Components/AboutUs';
 import ContactUs from './Components/ContactUs';
 import Header from './Components/Header';
@@ -10,7 +9,6 @@ import Footer from './Components/Footer';
 import ToastMessage from './Components/ToastMessage';
 import { useEffect, useState } from 'react';
 import Products from './Components/Products';
-import websiteDown from '../src/Images/websiteDown.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -36,10 +34,10 @@ function App() {
 
   return (
     <div className="App">
-      {/* <img src={websiteDown} width="100%" height="100%" alt="Website Maintenance" /> */}
       <Router>
-        {showMessage.success && <ToastMessage showMessage={showMessage} />}
-        {showMessage.error && <ToastMessage showMessage={showMessage} />}
+        {(showMessage.success || showMessage.error) && (
+          <ToastMessage showMessage={showMessage} />
+        )}
         
         <Header />
         
